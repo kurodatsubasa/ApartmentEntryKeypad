7261e5056ea07d81dbca4965dfdb64ad525539fc
Hai Le <35050699+kurodatsubasa@users.noreply.github.com>
2018-02-13 22:34:25 -0600

    FileIO: Remove unneccesary imports

--- src/program3/FileIO.java
+++ src/program3/FileIO.java
@@ -10,8 +10,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class FileIO {
 
